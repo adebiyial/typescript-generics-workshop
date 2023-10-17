fe79e7ef419f6c22fc515157970131e7c9d14a68
Adebiyi Adedotun <adebiyiadedotun9@gmail.com>
2023-10-17 12:36:37 +0100

    Complete exercise 17.5

--- src/04-generics-advanced/17.5-inference-inside-generic-functions.problem.ts
+++ src/04-generics-advanced/17.5-inference-inside-generic-functions.problem.ts
@@ -8,10 +8,12 @@ type Person = {
 
 export function remapPerson<Key extends keyof Person>(
   key: Key,
-  value: Person[Key],
+  value: Person[Key]
 ): Person[Key] {
   if (key === "birthdate") {
-    return new Date();
+    const res = new Date() as Person[Key]
+    return res
+    // return new Date() as Person[Key];
   }
 
   return value;
@@ -24,5 +26,5 @@ const name = remapPerson("name", "John Doe");
 type tests = [
   Expect<Equal<typeof date, Date>>,
   Expect<Equal<typeof num, number>>,
-  Expect<Equal<typeof name, string>>,
+  Expect<Equal<typeof name, string>>
 ];
